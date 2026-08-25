84fb2e395feebea2ca643afcb0bd8e4d7dc1dc92
Roxanne Krause <rlutz1@unm.edu>
2026-08-25 07:55:07 -0600

    grabbed canvas and loading webgl2 context

--- webgl/triangle.js
+++ webgl/triangle.js
@@ -1,10 +1,14 @@
+/** constants for readability */
+const ERROR_BOX = "error-box";
+const CANVAS = "demo-canvas";
+
 /** display an error in the error box*/
 function show_error(err) {
-  const error_box = document.getElementById("error-box")
+  const error_box = document.getElementById(ERROR_BOX);
   const error_text = document.createElement("p");
-  error_text.textContent = err
-  error_box.appendChild(error_text) 
-  console.log(error_text)
+  error_text.textContent = err;
+  error_box.appendChild(error_text) ;
+  console.log(error_text);
 }
 
 // testing error box
@@ -17,10 +21,35 @@ function show_error(err) {
 function build_triangle() {
   // testing try catch
   // throw new Error("testing!")
+  
+  // this is not necessary, but gives hint to VSCode
+  // that this is a canvas element 
+  /** @type (HTMLCanvasElement|null) */
+  const canvas = document.getElementById(CANVAS);
+
+  // likely a dom related loading error.
+  // js is already included at the bottom of the index.html 
+  // but a good check to have in place.
+  if (!canvas){
+    show_error("Likely canvas didn't load in time, be sure to have javascript loaded at end of HTML.");
+    return;
+  }
+
+  // create a drawing context for this canvas.
+  // can only have one per canvas.
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+  // webgl2 is really what should be used these days.
+  const gl = canvas.getContext("webgl2");
+  
+  // check to make sure the context was created correctly!
+  if (!gl) {
+    show_error("Something went wrong when creating webgl2 context!");
+    return; // for now, just stop this from working.
+  }
 }
 
 try {
-  build_triangle()
+  build_triangle();
 } catch (err) {
-  show_error(`JS Exception: ${err}`)
+  show_error(`JS Exception: ${err}`);
 }
