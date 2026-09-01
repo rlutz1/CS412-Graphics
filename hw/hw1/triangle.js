c1ef44fc9eb07ee47fccdfb4bdf0ed586ff0698a
Roxanne Krause <rlutz1@unm.edu>
2026-09-01 10:01:45 -0600

    hehe crappy square

--- hw/hw1/triangle.js
+++ hw/hw1/triangle.js
@@ -1,15 +1,17 @@
 // --- Triangle ---
 const positions = new Float32Array([
-   0.0,  1.0, 0.0,  // vertex 1
-  1.0, 0.0, 0.0,  // vertex 2
-   -1.0, 0.0, 0.0   // vertex 3
+    0.0,  1.0,   // vertex 1
+    1.0,  0.0,   // vertex 2
+   -1.0,  0.0,   // vertex 3
+    0.0, -1.0    // vertex 4
 ]);
 
 // acheives full rotation to all vertices as desired but
 // still with fade to black.
 const colors = new Float32Array([
-  1.0, 0.0, 0.0,  // g
+  1.0, 1.0, 1.0,  // g
   0.0, 1.0, 0.0,  // red -> blue -> black
+  0.0, 0.0, 1.0,   // blue -> green -> black
   0.0, 0.0, 1.0   // blue -> green -> black
 ]);
 
