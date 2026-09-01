f16d7cdae42b0c9ff9aa054629e0014afc3514c9
Roxanne Krause <rlutz1@unm.edu>
2026-09-01 13:13:04 -0600

    2 triangles

--- hw/hw1/triangle.js
+++ hw/hw1/triangle.js
@@ -8,20 +8,40 @@
 
 // turns back to triangle
 const positions = new Float32Array([
-    0.0,  1.0, 0.0,  // vertex 1
-    1.0,  0.0, 0.0,  // vertex 2
-   -1.0,  0.0, 0.0,  // vertex 3
-    0.0, -1.0, 0.0   // vertex 4
+    // -1.0,  1.0, 0.0,  // top left
+    -1.0, -1.0, 0.0, // bottom left
+
+    1.0, -1.0, 0.0,   // bottom right
+    0.0, 0.0, 0.0, // middle
+
+    // 1.0, -1.0, 0.0, // bottom right
+    -1.0, 0.5, 0.0 // top right
+    // 1.0,  1.0, 0.0,  // top right
+     
+    // -0.5, 0.5, 0.0,
+    // 0.0, 0.0, 0.0
+   
+    //  -1.0,  1.0, 0.0
+
+  //   0.0, -1.0, 0.0,   // vertex 4
+  //  -1.0,  0.0, 0.0,  // vertex 3
+  //   0.0, 0.0, 0.0,
+    
+  //   // 0.0, 0.0, 0.0,
+  //   0.0, 1.0, 0.0,
+  //    0.0, 1.0, 0.0
+    
 ]);
 
 
 // acheives full rotation to all vertices as desired but
 // still with fade to black.
 const colors = new Float32Array([
-  1.0, 0.0, 0.0,  // g
-  0.0, 1.0, 0.0,  // red -> blue -> black
-  0.0, 0.0, 1.0,   // blue -> green -> black
-  1.0, 0.0, 0.0   // blue -> green -> black
+  1.0, 0.0, 0.0,  // red
+  0.0, 1.0, 0.0,  // green
+  0.0, 0.0, 1.0,   // blue
+  1.0, 0.0, 0.0   // red
+  // 0.0, 1.0, 0.0
 ]);
 
 // 
