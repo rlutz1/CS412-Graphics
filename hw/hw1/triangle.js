a8cddd2e87673170b47acac257bb678744dfd543
Roxanne Krause <rlutz1@unm.edu>
2026-09-03 06:06:24 -0600

    finalize triangle colors

--- hw/hw1/triangle.js
+++ hw/hw1/triangle.js
@@ -1,70 +1,65 @@
-// square
-// const positions = new Float32Array([
-//     0.0,  1.0,   // vertex 1
-//     1.0,  0.0,   // vertex 2
-//    -1.0,  0.0,   // vertex 3
-//     0.0, -1.0    // vertex 4
-// ]);
-
-// turns back to triangle
+/*
+  4 inward pointing triangles to create a square. 
+  4 are used instead of 2 to create a specific color 
+  matching pattern.
+ */
 const positions = new Float32Array([
+    // bottom triangle
     -1.0, -1.0, 0.0, // bottom left
-    1.0, -1.0, 0.0,   // bottom right
-    0.0, 0.0, 0.0, // middle
+    1.0, -1.0, 0.0,  // bottom right
+    0.0, 0.0, 0.0,   // middle
 
-    // next, left triangle
-    -1.0, -1.0, 0.0, 
-    0.0, 0.0, 0.0, 
-    -1.0, 1.0, 0.0, // top left 
-
+    // left triangle
+    -1.0, -1.0, 0.0, // bottom left
+    -1.0, 1.0, 0.0,  // top left 
+    0.0, 0.0, 0.0,   // middle
+    
     // top triangle
-    1.0, 1.0, 0.0,
-    -1.0, 1.0, 0.0, 
-    0.0, 0.0, 0.0, 
+    -1.0, 1.0, 0.0, // top left
+    1.0, 1.0, 0.0,  // top right
+    0.0, 0.0, 0.0,  // middle
 
     // right triangle
-    1.0, 1.0, 0.0,
-    1.0, -1.0, 0.0, 
-    0.0, 0.0, 0.0, 
-    
+    1.0, -1.0, 0.0, // bottom right
+    1.0, 1.0, 0.0,  // top right
+    0.0, 0.0, 0.0,  // middle
 
-
-    // 1.0, 1.0, 0.0, // top right
-    
-    // -1.0, -1.0, 0.0 // bottom left?
 ]);
 
+/*
+  groups of 3 color vertices.
+  each group is the vertex colors of
+  one of the 4 trianges.
+ */
 
-// acheives full rotation to all vertices as desired but
-// still with fade to black.
+// declare some colors up here to ease the pain of changing.
+const bottom_left_color =  [1.0, 0.0, 1.0]
+const bottom_right_color = [1.0, 1.0, 0.0]
+const middle_color =       [0.0, 1.0, 1.0]
+const top_left_color = bottom_right_color // want these to match
+const top_right_color = bottom_left_color // want these to match
+
+
+// add all colors together to match each triangle
 const colors = new Float32Array([
-  1.0, 0.0, 0.0,  // red
-  0.0, 1.0, 0.0,  // green
-  0.0, 0.0, 1.0,   // blue
+  // bottom triangle
+  ...bottom_left_color,   // bottom left
+  ...bottom_right_color,  // bottom right
+  ...middle_color,        // middle
   
-  1.0, 0.0, 0.0,   // red
-  0.0, 0.0, 1.0, // blue
-  0.0, 1.0, 0.0, // green
+  // left triangle
+  ...bottom_left_color,   // bottom left
+  ...top_left_color,      // top left
+  ...middle_color,        // middle
   
-  1.0, 0.0, 0.0,   // red
-  0.0, 1.0, 0.0, // green
-  0.0, 0.0, 1.0, // blue
+  // top triangle
+  ...top_left_color,      // top left
+  ...top_right_color,     // top right
+  ...middle_color,        // middle
 
-  1.0, 0.0, 0.0,   // red
-  0.0, 1.0, 0.0, // green
-  0.0, 0.0, 1.0, // blue
-  // 0.0, 1.0, 0.0
+  // right
+  ...bottom_right_color,  // bottom right
+  ...top_right_color,     // top right
+  ...middle_color,        // middle
+
 ]);
-
-// 
-// const colors = new Float32Array([
-//   1.0, -1.0, -1.0,  // g
-//   -1.0, 1.0, -1.0,  // red -> blue -> black
-//   -1.0, -1.0, 1.0   // blue -> green -> black
-// ]);
-// this is something
-// const colors = new Float32Array([
-//   0.1, 0.6, 1.0,  // green -> red -> black
-//   1.0, 0.1, 0.6,  // red -> blue -> black
-//   0.6, 1.0, 0.1   // blue -> green -> black
-// ]);
