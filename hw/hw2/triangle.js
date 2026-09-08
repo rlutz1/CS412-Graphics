644f3bbb22417a6194bb91df671ca21eca2ec073
Roxanne Krause <rlutz1@unm.edu>
2026-09-08 17:20:59 -0600

    cleaning

--- hw/hw2/triangle.js
+++ hw/hw2/triangle.js
@@ -1,65 +1,26 @@
 /*
-  4 inward pointing triangles to create a square. 
-  4 are used instead of 2 to create a specific color 
-  matching pattern.
+  single triangle only for this
  */
 const positions = new Float32Array([
-    // bottom triangle
-    -1.0, 0.0, 0.0, // bottom left
-    1.0, 0.0, 0.0,  // bottom right
-    0.0, 1.0, 0.0,   // middle
-
-    // // left triangle
-    // -1.0, -1.0, 0.0, // bottom left
-    // -1.0, 1.0, 0.0,  // top left 
-    // 0.0, 0.0, 0.0,   // middle
-    
-    // // top triangle
-    // -1.0, 1.0, 0.0, // top left
-    // 1.0, 1.0, 0.0,  // top right
-    // 0.0, 0.0, 0.0,  // middle
-
-    // // right triangle
-    // 1.0, -1.0, 0.0, // bottom right
-    // 1.0, 1.0, 0.0,  // top right
-    // 0.0, 0.0, 0.0,  // middle
-
+    // triangle
+    -0.5, -0.5, 0.0, // bottom left
+     0.5, -0.5, 0.0,  // bottom right
+     0.0,  0.5, 0.0,   // top
 ]);
 
 /*
-  groups of 3 color vertices.
-  each group is the vertex colors of
-  one of the 4 trianges.
+  simple 3 vertex colors
  */
 
 // declare some colors up here to ease the pain of changing.
 const bottom_left_color =  [0.0, 0.0, 1.0]
 const bottom_right_color = [0.0, 1.0, 0.0]
-const middle_color =       [1.0, 0.0, 0.0]
-const top_left_color = bottom_right_color // want these to match
-const top_right_color = bottom_left_color // want these to match
+const top_color =          [1.0, 0.0, 0.0]
 
-
-// add all colors together to match each triangle
+// color the vertices
 const colors = new Float32Array([
-  // bottom triangle
+  // triangle
   ...bottom_left_color,   // bottom left
   ...bottom_right_color,  // bottom right
-  ...middle_color,        // middle
-  
-  // // left triangle
-  // ...bottom_left_color,   // bottom left
-  // ...top_left_color,      // top left
-  // ...middle_color,        // middle
-  
-  // // top triangle
-  // ...top_left_color,      // top left
-  // ...top_right_color,     // top right
-  // ...middle_color,        // middle
-
-  // // right
-  // ...bottom_right_color,  // bottom right
-  // ...top_right_color,     // top right
-  // ...middle_color,        // middle
-
+  ...top_color,        // middle
 ]);
