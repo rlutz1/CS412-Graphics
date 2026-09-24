10d87440a64b7414e0a383550f284fc0b7c5852c
Roxanne Krause <rlutz1@unm.edu>
2026-09-24 16:55:43 -0600

    cleaning vertices, still working

--- hw/hw3/point_samplers.js
+++ hw/hw3/point_samplers.js
@@ -41,6 +41,7 @@ function gen_sphere_points(
 
   vertices.push(center[0], center[1], r) // save the north polar cap
 
+  // generate the bands of the sphere
   for (let v_i = 1; v_i < v_steps; v_i++) { // for each vertical step
        // get our current v, which is the v in (h, v)
         const v = v_min + (v_i * v_step)
@@ -63,53 +64,72 @@ function gen_sphere_points(
    
     vertices.push(center[0], center[1], -r) // save the south polar cap
 
-    let last_pt_index = (vertices.length / 3) - 1 // 9
-    let lower_band = last_pt_index - h_steps // 5
+    // ==================================
+    //  GENERATE INDICES
+    // ==================================
 
-    // SET UP POLAR NORTH indices
+    let last_pt_index = (vertices.length / 3) - 1 // the LAST POINT in the list of vertices, so the south polar cap
+    let lower_band = last_pt_index - h_steps // the min index of the band just "below" the south cap
+
+    // set up the polar NORTH indices
+    // set them up in a triangular way, all connecting to the south pole:
+    //      \ | /
+    //   -- pole --
+    //      / | \
     for (let i = 1; i < h_steps; i++) { // for each horizontal step
-      indices.push(0, i, i + 1)
+                                // if h steps == 4, 
+      indices.push(0, i, i + 1) // generally: n. pole, 1, 2; n. pole, 2, 3; n. pole, 3, 4
     } // end loop
-    indices.push(0, h_steps, 1)
+    indices.push(0, h_steps, 1) // special case: final vertex wraps back to start: n. pole, 4, 1
 
-    // SET UP THE MIDDLE VERTICES (general case)
-    for (let band_index = 1; band_index < lower_band; band_index += h_steps) { // TODO: -1 is important
-      for (let inc = 0; inc < h_steps - 1; inc++) {
-        const bottom_left = band_index + inc // 1
-        const top_left = bottom_left + h_steps // 5
-        const top_right = bottom_left + h_steps + 1 // 6
-        const bottom_right = bottom_left + 1 // 2
+    // set up the middle banding indeces (general case)
+    // going to start from the upper band and "reach down" to the lower band below.
+    // going in the motion:
+    // bottom_left -- bottom_right
+    //      |       /    |
+    // top_left    -- top_right
+    for (let band_walker = 1; band_walker < lower_band; band_walker += h_steps) {  // walk through all bands until reach south pole
+      for (let i = 0; i < h_steps - 1; i++) { // walk across the band, with special case on final setup
+        const bottom_left = band_walker + i // the "bottom left" point
+        const top_left = bottom_left + h_steps // the "top left" point
+        const top_right = bottom_left + h_steps + 1 // "top right" point
+        const bottom_right = bottom_left + 1 // "bottom right" point
 
+        // add the two triangles this set of vertices makes.
         indices.push(
           bottom_left, top_left, top_right,
           bottom_left, bottom_right, top_right
         )
       } // end loop 
-      // special last case
-      const bottom_left = band_index + h_steps - 1 // 4
-      const top_left = bottom_left + h_steps // 8
-      const top_right = band_index + h_steps // 5
-      const bottom_right = band_index // 1
 
+      // special last case: we need to wrap around slightly
+      const bottom_left = band_walker + h_steps - 1 // final addition/point on band
+      const top_left = bottom_left + h_steps // still normal top left
+      const top_right = band_walker + h_steps // wrap around to the beginning of the lower band vertex
+      const bottom_right = band_walker // wrap around to the beginning of the band vertex
+
+      // push the final index
       indices.push(
           bottom_left, top_left, top_right,
           bottom_left, bottom_right, top_right
         )
     } // end loop
   
-    // SET UP THE POLAR SOUTH indices
+    // in same way we set up the north pol, we set up the SOUTH pole indeces
+    // with the last band.
     for (let i = lower_band; i < last_pt_index - 1; i++) {
       indices.push(last_pt_index, i, i + 1)
-    }
-    indices.push(last_pt_index, last_pt_index - 1, lower_band)
+    } // end loop
+    indices.push(last_pt_index, last_pt_index - 1, lower_band) // final point wrap around.
 
+  // return as a js dict
   return {
     "vertices": new Float32Array(vertices), 
     "indices": new Uint16Array(indices)
-  } // return as a js dict
-}
+  } 
+} // end method
 
-// testing only to ensure different colors to distinguish
+// just some fun colors to play around with.
 function gen_sphere_colors(num_vertices) {
   let sphere_color = []
 
@@ -128,13 +148,20 @@ function gen_sphere_colors(num_vertices) {
   } // end loop
 
   return new Float32Array(sphere_color)
-}
+} // end method
 
 
+/**
+ * cylinder point generation using parametric forms:
+ * x = x_c + r * cos(u)
+ * y = x_c + r * sin(u) 
+ * z = x_c + r * h * v
+ * 
+ */
 function gen_cylinder_points(
   r=1.5, // radius of the cylinder
   height=2.0, // height of the cylinder
-  center=[0, 0, 0], // center of the sphere
+  base=[0, 0, -1], // base of the cylinder
   h_step=0.1, // horizontal step interval (u)
   v_step=0.1, // vertical step interval (v)
   h_range=[0, (2 * Math.PI)], // the interval of the latitudinal point generation (horizontal, relatively)
@@ -150,12 +177,22 @@ function gen_cylinder_points(
   let h_steps = Math.round(Math.abs(h_max - h_min) / h_step)
   let v_steps = Math.round(Math.abs(v_max - v_min) / v_step)
 
+  // save for later use.
+  const base_x = base[0]
+  const base_y = base[1]
+  const base_z = base[2]
+
   const vertices = [] // simple js array for collection of vertices
   const indices = [] // js array for collecting indices
 
-  // top middle point
-  vertices.push(center[0], center[1], center[2]) // save the polar cap
+  // ==================================
+  //  GENERATE POINTS
+  // ==================================
 
+  // this is the base middle point
+  vertices.push(base_x, base_y, base_z) 
+
+  // generate the bands of the side of the cylinder.
   for (let v_i = 0; v_i <= v_steps; v_i++) { // for each vertical step
     // get our current v, which is the v in (h, v)
     const v = v_min + (v_i * v_step)
@@ -167,17 +204,21 @@ function gen_cylinder_points(
       const sin_h = Math.sin(h)
 
       // gather the points using parametric form
-      const x = center[0] + (r * cos_h)
-      const y = center[1] + (r * sin_h)
-      const z = center[2] + (height * v)
-      // console.log(x, y, z)
+      const x = base_x + (r * cos_h)
+      const y = base_y + (r * sin_h)
+      const z = base_z + (height * v)
+
       vertices.push(x, y, z) // save the points
     } // end loop
   } // end loop
   
   // bottom middle point, mimicking last z value created
-  // accounts for anything not quite reaching the actual height value.
-  vertices.push(center[0], center[1], vertices.at(-1)) // save the polar cap
+  // accounts for anything not quite reaching the actual height value (rounding issues cause lumps!).
+  vertices.push(base_x, base_y, vertices.at(-1)) // save the polar cap
+
+  // ==================================
+  //  GENERATE INDICES
+  // ==================================
 
   // set up the indices
   let last_pt_index = (vertices.length / 3) - 1 // 9
